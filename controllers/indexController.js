6f383de964431f71e1f4f6982cbb4c2b94d9476e
Josephina <joisewurld@gmail.com>
2016-06-25 18:57:45 -0400

    created new Topic page

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var router = express.Router();
 var User = require('../models/user.js');
+var Topic = require('../models/topic.js');
 
-
-router.post('/:username', function(req,res){
+// Post from Start
+router.post('/', function(req,res){
 		User.create(req.body,function(err, data){
-		res.redirect('/index/username')
+		res.redirect('/index/username');
 	})
 })
-
 
 // Start
 router.get('/', function(req,res)
@@ -18,9 +18,28 @@ router.get('/', function(req,res)
 });
 
 
+// index page
 router.get('/:username', function(req,res){
+	User.find({}, function(err,name){
 		res.render('index.html.ejs',{
+			list: name
 		});
+	})
+		
 });
+
+// Topic
+router.post('/topic', function(req,res)
+{
+	res.render('topic.html.ejs');
+
+});
+
+router.post('/topic/new', function(req,res){
+		Topic.create(req.body,function(err, data){
+		res.redirect('/index/username')
+	})
+})
+
 
 module.exports = router;
