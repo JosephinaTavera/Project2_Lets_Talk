df14f10d7a815a733eff30e9d092680002b77d0d
Josephina <joisewurld@gmail.com>
2016-06-29 00:06:29 -0400

    playing with comment section

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -29,6 +29,45 @@ router.get('/:username', function(req,res){
 		
 });
 
+// Comment
+router.post('/topic/:topicid/comment', function(req,res)
+{
+		res.redirect('/index/topic/:topicid/comment');
+});
+
+router.get('/topic/:topicid/comment', function(req,res){
+	console.log(req.body)
+	Topic.find({},function(err,topic){
+		res.render('comment.html.ejs', {
+			list: topic
+		})
+});
+});
+
+
+router.post('/topic/:topicid/comment/new', function(req,res){
+	// 	Comment.create(req.body,function(err, data){
+	// 	res.redirect('/topic/:topicid/comment')
+	// })
+	res.render('newcomment.html.ejs');
+})
+
+router.post('/topic/:topicid/comment/search', function(req,res){
+		// attempting search
+		//     phrase = '/' + req.body.title + '/i' ;
+		//     searchPhrase = {title: phrase};
+		//      console.log(searchPhrase);
+		// The following format does work
+		// Topic.find({title: /Raffle/i}, function(err, data){
+
+		Comment.find(req.body,function(err, data){
+			console.log(data);
+		res.redirect('/index/topic/:topicid/comment')
+	})
+})
+
+
+
 // Topic
 router.post('/topic', function(req,res)
 {
@@ -58,30 +97,5 @@ router.post('/topic/search', function(req,res){
 	})
 })
 
-// Comment
-router.post('/topic/:topicid/comment', function(req,res)
-{
-	console.log(topicid)
-	// Topic.find({},function(err,topic){
-	// 		list: topic
-		res.redirect('/index/topic/:topicid/comment');
-	// })
-});
-
-router.get('/topic/:topicid/comment', function(req,res){
-	console.log(req.body)
-	Topic.find({},function(err,topic){
-		res.render('comment.html.ejs', {
-			list: topic
-		})
-});
-});
-
-
-router.post('/topic/:topicid/comment/new', function(req,res){
-		Comment.create(req.body,function(err, data){
-		res.redirect('/topic/topicid/comment')
-	})
-})
 
 module.exports = router;
